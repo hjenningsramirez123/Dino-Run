e7b024a6268ba7895015b9458018700a7aa19284
Harry <h.jenningsramirez@digipen.edu>
2020-01-30 14:20:14 -0800

    Fresh copy

---
+++ Dino Runner/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,42 @@
+﻿/*********
+ * LevelGenerator.cs
+ * By: Harry Jennings-Ramirez & [blank]
+ * Last Edited: 1/30/2020
+ * Description: Generates terrain in front of the player, obstacles are created as well and are random
+ ********/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField] private Transform LevelPart;
+
+    private GameObject Player;
+    private Vector3 EndPosition;
+
+    private void Awake()
+    {
+
+
+    }
+
+    private void SpawnPart(Vector2 Spawn)
+    {
+        //Transform Instantiate(LevelPart, Spawn, Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Dino Runner/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public string Left = "a";
+    public string Right = "d";
+    public string Jump = "space";
+    public float Speed = 5;
+    public float JumpPower = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey(Left))
+        {
+            transform.position = transform.position + new Vector3(-Speed * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey(Right))
+        {
+            transform.position = transform.position + new Vector3(Speed * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey(Jump))
+        {
+            transform.position = transform.position + new Vector3(0, JumpPower * Time.deltaTime, 0);
+        }
+    }
+}
