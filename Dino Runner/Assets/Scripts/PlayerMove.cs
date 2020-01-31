97e453a40a6194bd227664f8a47a6d0d9fe3fb46
marvin-digipen <marvin.chan@digipen.edu>
2020-01-31 14:47:07 -0800

    Change Player Jump Mechanics

--- Dino Runner/Assets/Scripts/PlayerMove.cs
+++ Dino Runner/Assets/Scripts/PlayerMove.cs
@@ -8,7 +8,11 @@ public class PlayerMove : MonoBehaviour
     public string Right = "d";
     public string Jump = "space";
     public float Speed = 5;
-    public float JumpPower = 5;
+    public float JumpPower = 10;
+    public float FloatPower = 0.3f;
+    public float Gravity = 0.6f;
+    public float yValue = -2;
+    private float momentum = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,17 +24,35 @@ public class PlayerMove : MonoBehaviour
     {
         if(Input.GetKey(Left))
         {
-            transform.position = transform.position + new Vector3(-Speed * Time.deltaTime, 0, 0);
+            transform.position = transform.position + new Vector3(-Speed * Time.deltaTime, 0);
         }
 
         if(Input.GetKey(Right))
         {
-            transform.position = transform.position + new Vector3(Speed * Time.deltaTime, 0, 0);
+            transform.position = transform.position + new Vector3(Speed * Time.deltaTime, 0);
         }
-
         if(Input.GetKey(Jump))
         {
-            transform.position = transform.position + new Vector3(0, JumpPower * Time.deltaTime, 0);
+            if(transform.position.y < yValue + 0.0001f)
+            {
+                momentum = JumpPower;
+            }
+            else
+            {
+                momentum += FloatPower;
+            }
         }
+        print(momentum);
+        transform.position = transform.position + new Vector3(0, momentum * Time.deltaTime);
+        if(transform.position.y > yValue + 0.0001f)
+        {
+            momentum -= Gravity;
+        }
+        else
+        {
+            momentum = 0;
+            transform.position = new Vector3(transform.position.x, yValue);
+        }
+        
     }
 }
