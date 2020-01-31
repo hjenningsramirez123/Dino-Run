5f4aefb8070ad53ddcc2d80799409ecda5e14289
marvin-digipen <marvin.chan@digipen.edu>
2020-01-31 14:15:57 -0800

    add scroll method

---
+++ Dino Runner/Assets/Scripts/SideScroll.cs
@@ -0,0 +1,32 @@
+﻿/* SideScroll.cs
+ * Marvin Chan
+ * 31 Jan. 2020
+ * Manages sideways scrolling of terrain objects
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideScroll : MonoBehaviour
+{
+    public static float speed = 3f;
+    Rigidbody2D myRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(transform.position.x < -16)
+        {
+            transform.position = new Vector2(26, transform.position.y);
+        }
+        transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0);
+        
+    }
+}
