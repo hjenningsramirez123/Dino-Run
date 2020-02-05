975b92c67e6b30d89b5928f0f9dca778c8e5dec4
hjenningsramirez123 <h.jenningsramirez@digipen.edu>
2020-02-05 13:04:36 -0800

    Updated GameManager

--- Dino Runner/Assets/Scripts/GameManager.cs
+++ Dino Runner/Assets/Scripts/GameManager.cs
@@ -1,22 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private List<GameObject> terrainObjects;
     public static float ScrollSpeed = 10f;
     public static float TeleportDistance = 26f;
+    public Text Score;
+    public PlayerMove Player;
+    public float CurrentScore;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GetComponent<PlayerMove>();
+        CurrentScore = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("down"))
+        {
+            CurrentScore += ScrollSpeed;
+        }
+
+        Score.text = "00000" +  (int)CurrentScore;
     }
 }
